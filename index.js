6a070db464b7585269249b1335e19c02ee747852
makyia bonner <makyiabonner@gmail.com>
2022-12-30 13:07:23 -0500

    Deleting Error outputs and JS

--- index.js
+++
@@ -1,9 +0,0 @@
-let first = document.getElementById("first");
-let last = document.getElementById("last");
-let email = document.getElementById("email");
-let phone = document.getElementById("tele");
-let pass = document.getElementById("pass");
-let cpass = document.getElementById("cpass");
-
-
-//Password and 
