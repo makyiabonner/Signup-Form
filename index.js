ac2a8f14a095df4817bbb152fa2df1c43a495cc9
makyia bonner <makyiabonner@gmail.com>
2022-12-30 12:54:18 -0500

    Inserting inputs into JS

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-let name = document.getElementsByName("name");
+let first = document.getElementById("first");
+let last = document.getElementById("last");
+let email = document.getElementById("email");
+let phone = document.getElementById("tele");
+let pass = document.getElementById("pass");
+let cpass = document.getElementById("cpass");
+
+
